3dc293e630b87f1b4f4a882dff35b640dbbac6c6
pnminh <pnminh232@gmail.com>
2019-03-24 17:41:16 -0500

    add nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MeetingsComponent } from './meetings/meetings.component';
+import { UsersComponent } from './users/users.component';
+import { HeadersComponent } from './headers/headers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MeetingsComponent,
+    UsersComponent,
+    HeadersComponent
   ],
   imports: [
     BrowserModule
